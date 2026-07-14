2af12bb8e750ded8f07de010caef8b81bc54def0
nawonwoo01 <nawonwoo01@gmail.com>
2026-07-14 13:44:22 +0900

    Bump service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "se-calculator-2026-07-v1";
+const CACHE_NAME = "se-calculator-2026-07-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
