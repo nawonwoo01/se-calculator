e2d8687ca3b0181feca3d092fee03b00268632b9
nawonwoo01 <nawonwoo01@gmail.com>
2026-07-22 13:22:11 +0900

    Bump PWA cache for patient profiles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "se-calculator-2026-07-v4";
+const CACHE_NAME = "se-calculator-2026-07-v5";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
